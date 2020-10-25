c34fa5fc7c6a3d7b4afc8d79b0dc94e015fa99aa
VoAnhHuy123 <51107414+VoAnhHuy123@users.noreply.github.com>
2020-10-26 00:05:05 +0700

    d

--- conn.php
+++ conn.php
@@ -3,7 +3,7 @@
 $conn = new mysqli('remotemysql.com', 'MBrwe2KOAD', 's0gXVIzNks', 'MBrwe2KOAD');
 
 	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
+	    die("Connection failed1: " . $conn->connect_error);
 	}
 	
 ?>
