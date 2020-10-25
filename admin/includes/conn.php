3a345d536859393bff18656f5eb08714739b3412
VoAnhHuy123 <51107414+VoAnhHuy123@users.noreply.github.com>
2020-10-26 01:15:28 +0700

    fsd

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'apsystem');
+	$conn = new mysqli('remotemysql.com', '6noE5I5vHn', '43isyV3Vkw', '6noE5I5vHn');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
