509b8a502498e8a315694ffa85a3749d01ae8327
VoAnhHuy123 <51107414+VoAnhHuy123@users.noreply.github.com>
2020-10-26 00:42:00 +0700

    Update conn.php
    zx

--- conn.php
+++ conn.php
@@ -1,6 +1,6 @@
 <?php
 //	$conn = new mysqli('localhost', 'root', '', 'apsystem');
-$conn = new mysqli('remotemysql.com', 'MBrwe2KOAD', 's0gXVIzNks', 'MBrwe2KOAD');
+$conn = new mysqli('remotemysql.com', '6noE5I5vHn', '43isyV3Vkw', '6noE5I5vHn');
 
 	if ($conn->connect_error) {
 	    die("Connection failed1: " . $conn->connect_error);
