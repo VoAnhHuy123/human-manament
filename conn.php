ba348074ee4c6be71c1496be319d48e5ea13e930
unknown <huyvo2581999@gmail.com>
2020-10-25 23:34:17 +0700

    1

--- conn.php
+++ conn.php
@@ -1,5 +1,6 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'apsystem');
+//	$conn = new mysqli('localhost', 'root', '', 'apsystem');
+$conn = new mysqli('remotemysql.com', 'MBrwe2KOAD', 's0gXVIzNks', 'MBrwe2KOAD');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
